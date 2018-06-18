775dc0f2c4393079ccb596edae8bc15737a95162
Bill Zissimopoulos <billziss@navimatics.com>
2018-06-18 14:59:02 -0700

    fix issue #4

--- make.cmd
+++ make.cmd
@@ -3,6 +3,6 @@
 set PATH=C:\Program Files\mingw-w64\x86_64-6.3.0-win32-seh-rt_v5-rev2\mingw64\bin;%PATH%
 set CPATH=C:\Program Files (x86)\WinFsp\inc\fuse
 
-for /f %%d in ('powershell "Get-Date -UFormat %%y%%j"') do set MyBuildNumber=%%d
+for /f %%d in ('powershell -NoProfile -NonInteractive -ExecutionPolicy Unrestricted "Get-Date -UFormat %%y%%j"') do set MyBuildNumber=%%d
 
 mingw32-make MyBuildNumber=%MyBuildNumber% %*
